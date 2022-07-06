15da27fa34aff75e986a43f3f8d715fb0e25a223
mukstoo <rodrigogreconunes@gmail.com>
2022-07-06 11:15:11 -0300

    border color change

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 const circle = document.querySelector('circle');
-const perimeter = circle.getAttribute('r')*2*Math.PI;
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 let currentOffset = 0
 let duration;
+let strokeHSLColor;
+let colorIncrement;
+let currentHue;
 const timer = new Timer(durationInput, startButton, pauseButton, {
     onStart(totalDuration) {
         duration = totalDuration;
     },
     onTick(timeRemaining) {
-        circle.setAttribute('stroke-dashoffset', perimeter*timeRemaining/duration-perimeter);
-        /* currentOffset = currentOffset-1; */
+        circle.setAttribute('stroke-dashoffset', perimeter * timeRemaining / duration - perimeter);
+        strokeHSLColor = circle.getAttribute('stroke');
+        colorIncrement = 180 / (duration * 100);
+        currentHue = strokeHSLColor.split('(');
+        currentHue = currentHue[1].split(',');
+        currentHue = currentHue[0] - colorIncrement;
+        circle.setAttribute('stroke', `hsl(${currentHue},100%,50%)`);
     },
     onComplete() {
-        console.log('completed');
+        circle.setAttribute('stroke', 'hsl(180,100%,50%)');
     }
 });
