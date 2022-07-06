2c1a296202fca2e85696a33510c2553982d618b8
mukstoo <rodrigogreconunes@gmail.com>
2022-07-06 00:30:17 -0300

    working prototype

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello world');
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
+const perimeter = circle.getAttribute('r')*2*Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+let currentOffset = 0
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+        duration = totalDuration;
+    },
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset', perimeter*timeRemaining/duration-perimeter);
+        /* currentOffset = currentOffset-1; */
+    },
+    onComplete() {
+        console.log('completed');
+    }
+});
